785bba100ca4c321c451b67bb39dc74c1c77b40c
jamietech <jamie@jamietech.co.cc>
2012-11-05 18:27:43 +1000

    Add user input listener.

--- src/tk/nekotech/dev/soaringcats/Main.java
+++ src/tk/nekotech/dev/soaringcats/Main.java
@@ -1,7 +1,30 @@
 package tk.nekotech.dev.soaringcats;
 
-public class Main {
+import java.util.Scanner;
+
+public class Main extends Thread {
+    private static SoaringCats sc;
+
     public static void main(final String[] args) {
-        new SoaringCats();
+        Main.sc = new SoaringCats();
+    }
+
+    @Override
+    public void run() {
+        final Scanner scan = new Scanner(System.in);
+        while (true) {
+            final String next = scan.nextLine();
+            if (next.equalsIgnoreCase("stop")) {
+                Main.sc.quitServer("Stopping! Meow~~");
+                Main.sc.dispose();
+                try {
+                    Thread.sleep(1500); // Sleep for quit to process.
+                } catch (final InterruptedException e) {
+                }
+                System.exit(1);
+            } else {
+                Main.sc.sendRawLineViaQueue(next);
+            }
+        }
     }
 }
